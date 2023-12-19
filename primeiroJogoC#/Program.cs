ca1a0e4c4f7e3162213df575c47b8f9f8f51d2d6
JoiceAlves0102 <98896167+JoiceAlves0102@users.noreply.github.com>
2023-12-19 17:14:59 -0300

    Add files via upload

---
+++ primeiroJogoC#/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Bem vindo ao Jogo de adivinhação de números, sua tarefa é escolher um número entre 1 e 10");
+
+
+Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 10);
+
+do
+{
+    Console.WriteLine("Digite o número escolhido");
+    int numeroEscolhido = int.Parse(Console.ReadLine()!);
+
+    if (numeroEscolhido == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns você acertou o número secreto");
+        break;
+    }
+    else if (numeroEscolhido < numeroSecreto)
+    {
+        Console.WriteLine("O número que você escolheu é menor que o número secreto");
+    }
+    else
+    {
+        Console.WriteLine("O número que você escolheu é maior que o número secreto");
+    }
+} while (true);
+
+Console.WriteLine("\nO jogo acabou. Você acertou o número secreto :) ");
